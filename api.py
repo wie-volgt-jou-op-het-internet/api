c15f764f8d112f2c6d94d245d1a0ae3744732246
Sander Van Damme <6276608-sander-vd@users.noreply.gitlab.com>
2022-11-26 15:51:39 +0100

    changes

--- api.py
+++ api.py
@@ -2,18 +2,22 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask_cors import CORS
 import redis
 import time
 
 app = Flask(__name__)
+CORS(app)
+
 r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 
 initial_state = {
+    'time': '0',
+    'count': '0',
     'Google': '0',
     'Facebook': '0',
     'Microsoft': '0',
-    'Amazon': '0',
-    'count': '0'
+    'Amazon': '0'
 }
 
 
@@ -28,14 +32,15 @@ def get_state():
     tracker_states = {}
     for key in initial_state.keys():
         tracker_states[key] = r.get(key)
+    tracker_states['time'] = int(time.time())
     return jsonify(tracker_states)
 
 
 @app.route('/v1/set_state', methods=['GET'])
 def set_state():
     name = request.args.get('name')
-    r.set(name, int(time.time()))
     r.set('count', int(r.get('count')) + 1)
+    r.set(name, int(time.time()))
     return jsonify({})
 
 
